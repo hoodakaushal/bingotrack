fac02451fdf012f43296c5c6d6d180dda8ee718b
unknown <hoodakaushal@gmail.com>
2018-04-02 19:36:25 +0530

    Create basic classes

--- src/main/java/hooda/bingo/Book.java
+++ src/main/java/hooda/bingo/Book.java
@@ -1,4 +1,18 @@
 package hooda.bingo;
 
+import lombok.Getter;
+
+import java.net.URL;
+import java.util.List;
+
+@Getter
 public class Book {
+    //Need this in case of multiple books of same name
+    private int id;
+    private String name;
+    private String author;
+    private URL goodreadsUrl;
+
+    private List<SquareWithMode> applicableSquares;
+    
 }
